09c4b81b193fcd99c53c7d7b5ba6abda2175100d
mohammedfirdouss <mohammedfirdousaraoye@gmail.com>
2025-06-26 08:38:04 +0000

    remove bash input for now

--- bin/login_aws-cli.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/bash
-
-CYAN='\033[1;36m'
-NO_COLOR='\033[0m'
-LABEL="Caller Identity"
-
-IDENTITY=$(aws sts get-caller-identity --output json)
-printf "${CYAN}==== ${LABEL} ====\n${NO_COLOR}"
-echo "$IDENTITY"
