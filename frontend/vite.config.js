86cf165dd46342b3d638dce26482392e2d64ae35
Mohammed Firdous <124298708+mohammedfirdouss@users.noreply.github.com>
2025-07-28 11:04:04 +0100

    Fix @ant-design/icons export error

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -16,6 +16,7 @@ export default defineConfig({
     global: 'window',
   },
   optimizeDeps: {
+    exclude: ['@ant-design/icons'],
     esbuildOptions: {
       plugins: [
         NodeGlobalsPolyfillPlugin({
