073a0d3cb7bae8bb2397eb4d947d3a6f508b189c
Mohammed Firdous <124298708+mohammedfirdouss@users.noreply.github.com>
2025-07-28 11:07:24 +0100

    Fix classnames module export error

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -17,6 +17,7 @@ export default defineConfig({
   },
   optimizeDeps: {
     exclude: ['@ant-design/icons'],
+    include: ['classnames'],
     esbuildOptions: {
       plugins: [
         NodeGlobalsPolyfillPlugin({
